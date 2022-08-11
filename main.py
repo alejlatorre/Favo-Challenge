4d9054fab81215d2f2f5d4eb5e7d002b8a1bdf94
alejlatorre <alejandro.latorre@pucp.edu.pe>
2022-08-11 00:55:00 -0500

    added plots in code and outputs

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 import dtale
 import warnings
 import numpy as np
-import pandas as pd 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 from src.utils import Plotly_Plots
 
@@ -15,6 +17,10 @@ option_settings = {
     'display.float_format': '{:,.4f}'.format
 }
 [pd.set_option(setting, option) for setting, option in option_settings.items()]
+
+%matplotlib inline
+plt.rcParams["figure.autolayout"] = True
+sns.set_theme(style='darkgrid')
 
 IN_PATH = 'data/in/'
 OUT_PATH = 'data/out/'
@@ -63,8 +69,8 @@ dtale.instances()
 # 26. subcategory: Subcategory 
 
 # %% Barplot per region_id
-PP = Plotly_Plots(df=data, dim_col='region_id', dim_title='Region')
-PP.barplot_plotly()
+# PP = Plotly_Plots(df=data, dim_col='region_id', dim_title='Region')
+# PP.barplot_plotly()
 
 # %% 4. Processing
 data = data_.copy()
@@ -77,7 +83,7 @@ data.drop(columns=cols_to_drop, inplace=True)
 
 data_orders = pd.pivot_table(
     data=data,
-    index=['order_number', 'region_id', 'store_id', 'buyer_id', 'payment_type', 'purchase_completed'],
+    index=['created_date', 'effective_date_time', 'order_number', 'region_id', 'store_id', 'buyer_id', 'payment_type', 'purchase_completed'],
     values=[
         'full_price', 'discount', 'discounted_price', 'quantity', 'coupon_discount', 
         'total_full_price', 'total_discounted_price'
@@ -101,6 +107,11 @@ data_orders.rename(columns=rename_cols, inplace=True)
 data_orders['avg_full_price_per_product'] = data_orders['total_full_price'] / data_orders['quantity']
 data_orders['avg_disc_price_per_product'] = data_orders['total_discounted_price'] / data_orders['quantity']
 data_orders['avg_disc_per_product'] = data_orders['total_discount'] / data_orders['quantity']
+to_map = {
+    'Yes': 1,
+    'No': 0
+}
+data_orders['purchase_completed'] = data_orders['purchase_completed'].map(to_map)
 
 payment_type_2 = pd.pivot_table(
     data=data_orders[data_orders['region_id'] == '2'],
@@ -126,5 +137,150 @@ payment_type_2['orders_cum_prc'] = payment_type_2['order_id'].cumsum() / payment
 payment_type_6['orders_cum_prc'] = payment_type_6['order_id'].cumsum() / payment_type_6['order_id'].sum() 
 payment_type_2['revenue_cum_prc'] = payment_type_2['total_discounted_price'].cumsum() / payment_type_2['total_discounted_price'].sum() 
 payment_type_6['revenue_cum_prc'] = payment_type_6['total_discounted_price'].cumsum() / payment_type_6['total_discounted_price'].sum() 
+payment_type_2.reset_index(inplace=True)
+payment_type_6.reset_index(inplace=True)
+
+base_ts_2 = pd.pivot_table(
+    data=data_orders[data_orders['region_id']=='2'],
+    index='created_date',
+    values=[
+        'order_id', 'purchase_completed', 'quantity', 
+        'total_discounted_price', 'total_full_price', 'total_discount', 
+        'avg_full_price_per_product', 'avg_disc_per_product'
+    ],
+    aggfunc={
+        'order_id': pd.Series.nunique,
+        'purchase_completed': np.sum,
+        'quantity': np.mean,
+        'total_full_price': np.sum,
+        'total_discount': np.sum,
+        'total_discounted_price': np.sum,
+        'avg_full_price_per_product': np.mean,
+        'avg_disc_per_product': np.mean
+    }
+).reset_index()
+base_ts_2['day_name'] = base_ts_2['created_date'].dt.day_name()
+
+base_ts_6 = pd.pivot_table(
+    data=data_orders[data_orders['region_id']=='6'],
+    index='created_date',
+    values=[
+        'order_id', 'purchase_completed', 'quantity', 
+        'total_discounted_price', 'total_full_price', 'total_discount', 
+        'avg_full_price_per_product', 'avg_disc_per_product'
+    ],
+    aggfunc={
+        'order_id': pd.Series.nunique,
+        'purchase_completed': np.sum,
+        'quantity': np.mean,
+        'total_full_price': np.sum,
+        'total_discount': np.sum,
+        'total_discounted_price': np.sum,
+        'avg_full_price_per_product': np.mean,
+        'avg_disc_per_product': np.mean
+    }
+).reset_index()
+base_ts_6['day_name'] = base_ts_6['created_date'].dt.day_name()
+
+# %% 5. Plots
+mask_region_2 = data_orders['region_id'] == '2'
+mask_region_6 = data_orders['region_id'] == '6'
+
+# Countplot: Qty of trx
+fig, axes = plt.subplots(2, 1, figsize=(10, 10))
+sns.countplot(
+    y='payment_type', 
+    data=data_orders[mask_region_2],
+    order=data_orders.loc[mask_region_2, 'payment_type'].value_counts().index,
+    ax=axes[0]
+)
+sns.countplot(
+    y='payment_type', 
+    data=data_orders[mask_region_6],
+    order=data_orders.loc[mask_region_6, 'payment_type'].value_counts().index,
+    ax=axes[1]
+)
+fig.suptitle('Número de ordenes por tipo de pago y región', fontsize=20)
+axes[0].set(xlabel='Numero de ordenes', ylabel='Tipo de pago', title='Region 2')
+axes[1].set(xlabel='Numero de ordenes', ylabel='Tipo de pago', title='Region 6')
+plt.show()
+
+# Barplot: Total discounted price
+fig, axes = plt.subplots(2, 1, figsize=(10, 10))
+sns.barplot(
+    y='payment_type', 
+    x='total_discounted_price',
+    data=payment_type_2,
+    ax=axes[0]
+)
+sns.barplot(
+    y='payment_type',
+    x='total_discounted_price',
+    data=payment_type_6,
+    ax=axes[1]
+)
+fig.suptitle('Revenue por tipo de pago y región', fontsize=20)
+axes[0].set(xlabel='Revenue', ylabel='Tipo de pago', title='Region 2')
+axes[1].set(xlabel='Revenue', ylabel='Tipo de pago', title='Region 6')
+plt.show()
+
+# Lineplot: Trx evolution
+fig, ax1 = plt.subplots(1, 1, figsize=(10, 4))
+ax2 = ax1.twinx()
+ax1.plot(base_ts_2[['created_date', 'order_id']].set_index('created_date'), color='g')
+ax2.plot(base_ts_6[['created_date', 'order_id']].set_index('created_date'), color='b')
+ax1.set_xlabel('Día')
+ax1.set_ylabel('Ordenes (Region 2)')
+ax2.set_ylabel('Ordenes (Region 6)')
+fig.legend(['Region 2', 'Region 6'])
+fig.suptitle('Evolución de ordenes diarias por region')
+fig.autofmt_xdate(rotation=45)
+plt.show()
+# fig = px.line(
+#     data_frame=base_ts, 
+#     x='created_date', 
+#     y='order_id',
+#     width=900,
+#     height=350,
+#     title='Evolución de ordenes diarias'
+# )
+# fig.update_layout(
+#     title='Evolución de ordenes diarias',
+#     xaxis_title='Días',
+#     yaxis_title='Número de ordenes',
+#     title_x=0.5
+# )
+# fig.show()
+
+# Barplot: Avg trx per day
+plt.figure(figsize=(6, 4))
+sns.barplot(
+    x='day_name', 
+    y='order_id',
+    data=base_ts.groupby(['day_name'])['order_id'].mean().sort_values(ascending=False).reset_index(),
+)
+plt.xlabel('Dia de la semana')
+plt.ylabel('Promedio de ordenes')
+plt.title('Promedio de ordenes por dia de la semana', fontsize=15)
+plt.show()
+
+# Frecuencia
+data_orders.order_id.nunique() / data_orders.buyer_id.nunique()
+
+# MAUs
+data_orders.buyer_id.nunique()
+
+# MRPU
+data_orders.total_discounted_price.sum() / data_orders.buyer_id.nunique()
+
+# Trx
+data_orders.order_id.nunique()
 
 # %%
+
+
+pd.pivot_table(
+    data=data_orders,
+    index='created_date',
+    values=['order_id']
+)
